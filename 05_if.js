cb9f503797a239d1deea383b22dd2aa7ef01bbe8
吕长峰 <2523806339@qq.com>
2019-07-13 17:02:32 +0800

    add js file

---
+++ 05_if.js
@@ -0,0 +1,25 @@
+//满30减12
+//如果订单金额满30，在原来的基础上减12
+//var total=32;
+//if(total>=30){
+//  total-=12;
+//}
+//console.log(total);
+//声明变量保存年龄，如果满18，打印成年人。
+//var age=23;
+//if(age>=18){
+	//console.log("成年人");
+//};
+//console.log("未成年人");
+//false;0 NaN '' undefined null-在条件表达式中
+//练习：声明变量保存个人签名，如果签名内容为空，给变量赋值'这家伙很懒什么也没留下'；最后打印变量
+//var a="";
+//if(!a){
+	//a='这家伙很懒什么也没留下';
+//}
+//console.log(a);
+var age=19;
+if(age>=18){
+	console.log('成年人');
+}
+	console.log('未成年人');
